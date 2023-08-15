597fc9fde7e4048a4747501d3c4621f3ed12e78e
suhailsait <suhailsait22@gmail.com>
2023-08-15 17:09:34 +0530

    footer-update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,7 +36,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    scrollPositionRestoration: 'enabled'
+    scrollPositionRestoration: 'enabled',
+    useHash: true
   })],
   exports: [RouterModule]
 })
